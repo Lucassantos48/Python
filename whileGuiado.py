20671a3ce1d7931e00335548ace7617f06e032cf
Lucassantos48 <lucasmsantos049@gmail.com>
2024-09-02 18:38:31 -0300

    Utilizando While

---
+++ whileGuiado.py
@@ -0,0 +1,14 @@
+nome = 'Luiz Otavio' #Strings são itaraveis
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'# {letra}'
+    indice += 1
+
+    
+    print(novo_nome)
+
+
+
